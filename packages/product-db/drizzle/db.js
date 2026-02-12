56e064cec847cf25be0f4e56feae4fe19383f16d
Priyanshu <work.priyanshu085@gmail.com>
2026-02-12 12:11:31 +0530

    refactor: remove obsolete database and schema files; add TypeScript configuration for product-db and types packages.

--- packages/product-db/drizzle/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = exports.DATABASE_URL = void 0;
-var serverless_1 = require("@neondatabase/serverless");
-var neon_http_1 = require("drizzle-orm/neon-http");
-var schema = require("./schema");
-exports.DATABASE_URL = process.env.DATABASE_URL || "";
-var sql = (0, serverless_1.neon)(exports.DATABASE_URL);
-exports.db = (0, neon_http_1.drizzle)(sql, { schema: schema });
