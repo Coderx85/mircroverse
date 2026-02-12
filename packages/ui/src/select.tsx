6b0f88a7ffc4b8440f2627b9814d36bcdbbac9e0
Priyanshu <work.priyanshu085@gmail.com>
2026-02-12 12:11:13 +0530

    refactor: remove outdated documentation files and update import paths in components.

--- packages/ui/src/select.tsx
+++ packages/ui/src/select.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 
-import { cn } from "@/lib/utils";
+import { cn } from "./lib/utils";
 import { IconCheck, IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 
 const Select = SelectPrimitive.Root;
